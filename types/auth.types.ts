0f03c1a86518d027cdfde21514aad5403df13b01
Anmol Vishwakrma <anmol.vishwakrma@cloudanalogy.com>
2026-05-22 16:09:11 +0530

    Next auth implement, Signup page connected

--- types/auth.types.ts
+++ types/auth.types.ts
@@ -1,1 +1,27 @@
-export type AuthMode = 'login' | 'signup'
+import { DefaultSession } from "next-auth";
+
+export type AuthMode = 'login' | 'signup';
+
+export interface AppSession extends DefaultSession {
+  user: DefaultSession["user"] & {
+    id: string;
+  };
+}
+
+export type AppJWT = {
+  id?: string;
+};
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & {
+      id: string;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id?: string;
+  }
+}
