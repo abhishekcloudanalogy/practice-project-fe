05fdb555374fe8ddf63661b4cde48a19f51afd9d
Arvind Gautam <arvind.gautam@cloudanalogy.com>
2026-06-19 13:50:29 +0530

    Fixed as per requirement

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 import Sidebar from '@/components/layout/Sidebar'
-<<<<<<< HEAD
-import DashboardHeader from '@/components/layout/DashboardHeader'
-import Navbar from '@/components/layout/Navbar'
-
-=======
 import { SidebarProvider } from '@/store/features/dashboard/sidebarContext'
 import NavbarGuard from '@/components/layout/NavbarGuard'
->>>>>>> b7fb3a9 (Fixed as per requirement)
 
 type DashboardLayoutProps = {
   children: ReactNode
@@ -21,24 +15,6 @@ export const metadata: Metadata = {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-<<<<<<< HEAD
-    <div className="min-h-[calc(100vh-var(--navbar-height))] bg-slate-50 text-slate-900">
-      <Navbar />
-      <Sidebar />
-
-      <main
-        className="w-full transition-[padding-left] duration-200 pl-0 md:pl-[var(--sidebar-width,92px)]"
-        style={{
-          paddingTop: 'var(--navbar-height)',
-          minHeight: 'calc(100vh - var(--navbar-height) )',
-        }}
-      >
-        <div className="overflow-x-hidden">
-          {children}
-        </div>
-      </main>
-    </div>
-=======
     <SidebarProvider>
       <div className="min-h-[calc(100vh-var(--navbar-height))] bg-slate-50 text-slate-900">
         <NavbarGuard  >
@@ -56,6 +32,5 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
         </NavbarGuard>
       </div>
     </SidebarProvider>
->>>>>>> b7fb3a9 (Fixed as per requirement)
   )
 }
